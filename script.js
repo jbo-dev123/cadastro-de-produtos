050a288127f16cde1dc80c0f6d332fcc64f01278
jbo-dev123 <johnnybragueto@hotmail.com>
2025-01-22 20:24:00 -0300

    add as referencias aos elementos de html no JS e add um array vazio  products onde cada um deles sera armazenado como um objeto

---
+++ script.js
@@ -0,0 +1,8 @@
+const formContainer = document.getElementById('form-container')
+const listContainer = document.getElementById('list-container')
+const productList =  document.getElementById('product-list')
+const formProdutos = document.getElementById('form-produtos')
+const addProduto = document.getElementById('add-produto')
+
+const products = [];
+
