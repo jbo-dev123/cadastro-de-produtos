b3bdb33dedb827df81cd5c0aea3800f21af737c8
jbo-dev123 <johnnybragueto@hotmail.com>
2025-01-22 21:24:38 -0300

    adicionei uma function event, um preventDefault, um push e criei algumas const para pegar o valor das variaveis

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ const addProduto = document.getElementById('add-produto')
 
 const products = [];
 
+formProdutos.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome-produto').value;
+    const descricao = document.getElementById('descricao').value;
+    const numero = parseFloat(document.getElementById('value').value);
+    const disponivel = document.querySelector('input[name = "disp"]:checked').value;
+
+    products.push({nome, descricao, numero, disponivel})
+    
+})
